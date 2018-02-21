aecb3f660508323b6332cdc9c47c75c218de5bb6
Nina Rahimi <nina.i.rahimi@gmail.com>
2018-02-21 17:20:58 -0500

    Pushing to Trent , so that he can review the function

---
+++ factoryfunction.js
@@ -0,0 +1,36 @@
+/* Factory Function Lord of the Rings */
+
+createCharacter(name,nickName, race, origin, attack, defense) {
+    return{
+       name, 
+       nickName,
+       race,
+       origin,
+       attack,
+       defense,
+       describe: function(){
+            console.log(`${this.name} is a ${this.race} from ${this.origin}.`);
+       }
+    }
+}  
+ 
+     
+    //    ,
+    //    evaluateFight: function(character){
+    //        let x = this.attack - character.defense;
+    //        let y = character.attack - this.defense;
+    //        if(x < 0){
+    //            let x = 0;
+    //        }else if(y < 0){
+    //            let y = 0; 
+    //        }
+    //        console.log(`Your opponent takes ${x} damage and you receive ${y} damage`)
+    //    } 
+   
+  
+
+
+
+let character1 = createCharacter('Gandalf the White', 'gandalf', 'Wizard', 'Middle Earth', 10, 6)
+
+console.log(character1);
